97f5a1ed2678e794aa7685c4e96edb5580176a17
Will <w.stolton@protonmail.com>
2021-10-28 18:13:42 +0100

    now following pills

--- src/pacman/entries/pacman/WillStoltonPacman.java
+++ src/pacman/entries/pacman/WillStoltonPacman.java
@@ -5,10 +5,15 @@ import pacman.game.Constants.MOVE;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.DM;
 import pacman.game.Game;
+import pacman.game.internal.Ghost;
+
+import java.util.ArrayList;
 
 public class WillStoltonPacman extends Controller<MOVE> {
 
     private MOVE myMove= MOVE.NEUTRAL;
+
+    private int previousLocation = 0;
 
     public MOVE getMove(Game game, long timeDue)
     {
@@ -33,11 +38,12 @@ public class WillStoltonPacman extends Controller<MOVE> {
             // if the ghosts are not edible, we need to run so check if it's in the lair
             if(game.getGhostLairTime(ghost) == 0){
                 // if it isn't in the lair, it's after Ms P.
-                if (game.getShortestPathDistance(game.getGhostCurrentNodeIndex(ghost), msPacmanLocation) < 20){
+                if (game.getShortestPathDistance(game.getGhostCurrentNodeIndex(ghost), msPacmanLocation) < 40){
                     // if the ghost in question is closer than 10, we need to evade it
                     return game.getNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(ghost),
-                            game.getNeighbouringNodes(msPacmanLocation), DM.PATH);
+                            previousLocation, DM.PATH);
                 }
+                //TODO: look at previous location use above.
             }
         }
 
@@ -50,8 +56,36 @@ public class WillStoltonPacman extends Controller<MOVE> {
 
 
         // eat pills
+        int[] pills = game.getActivePillsIndices();
+        int[] powerUps = game.getActivePowerPillsIndices();
 
-        return myMove;
+        ArrayList<Integer> nextPill = new ArrayList<>();
+
+        for(int i = 0; i < pills.length; i++){
+            if(game.isPillStillAvailable(i)){
+                nextPill.add(pills[i]);
+            }
+        }
+        for(int i = 0; i < powerUps.length; i++){
+            if(game.isPowerPillStillAvailable(i)){
+                nextPill.add(powerUps[i]);
+            }
+        }
+
+        int[] targets = new int[nextPill.size()];
+
+        for(int i = 0; i < targets.length; i++){
+            targets[i] = nextPill.get(i);
+        }
+
+
+
+
+
+
+        // set msPacman's previous location at the end of the go. This will possibly be used for running away
+        previousLocation = msPacmanLocation;
+        return game.getNextMoveTowardsTarget(msPacmanLocation, targets[0], DM.PATH);
     }
 
 }
