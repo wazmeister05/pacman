a3e96c0995809b7e32010d1c2247fe84c1da7cdb
Will <w.stolton@protonmail.com>
2021-11-04 17:02:41 +0000

    right, still broken but less to be done to fix it

--- src/pacman/entries/pacman/WillStoltonPacman.java
+++ src/pacman/entries/pacman/WillStoltonPacman.java
@@ -96,6 +96,115 @@ public class WillStoltonPacman extends Controller<MOVE> {
         return search(game.copy(), msPLocation, allPills, allPowerPills, ghosts);
     }
 
+
+    /*
+    public MOVE getMove(Game game, long timeDue)
+    {
+        // We'll need this a lot throughout so making it easy.
+        int msPLocation = game.getPacmanCurrentNodeIndex();
+        // Want to avoid this position if a ghost is about to leave the lair.
+        int lairNodeIndex = game.getGhostInitialNodeIndex();
+        //
+        AVOID LAIR if a ghost is about to spawn.
+        //
+        for(GHOST ghost: GHOST.values()){
+            if(game.getGhostLairTime(ghost) < 1 && Arrays.asList(game.getNeighbouringNodes(msPLocation)).contains(lairNodeIndex)){
+                return game.getNextMoveAwayFromTarget(lairNodeIndex, msPLocation, DM.PATH);
+            }
+        }
+        //
+        RUN AWAY.
+        //
+        // Need to avoid ghosts first and foremost. No point doing everything else if there are ghosts nearby.
+        GHOST closestDangerousGhost = null;
+        int ghostDist = 10;
+        for(GHOST ghost: GHOST.values()){
+            // If the ghost is still in the lair, we can ignore it.
+            if(game.getGhostLairTime(ghost) == 0 && game.getGhostEdibleTime(ghost) == 0){
+                // If it isn't in the lair, it's after Ms P.
+                if (game.getShortestPathDistance(game.getGhostCurrentNodeIndex(ghost), msPLocation) < ghostDist){
+                    closestDangerousGhost = ghost;
+                    ghostDist = game.getShortestPathDistance(game.getGhostCurrentNodeIndex(ghost), msPLocation);
+                }
+            }
+        }
+        // If there is a closest ghost, run away from it. Now.
+        if(closestDangerousGhost != null){
+            return game.getNextMoveAwayFromTarget(game.getGhostCurrentNodeIndex(closestDangerousGhost), msPLocation, DM.PATH);
+        }
+        //
+        EAT GHOST - VERY similar to running away.
+        //
+        // If we've eaten a power pill, ghosts can be consumed, so lets go for them.
+        GHOST closestEdibleGhost = null;
+        int closestGhostDistance = Integer.MAX_VALUE;
+        for(GHOST ghost: GHOST.values()){
+            // if the ghost has more than 0 seconds being edible, head to it
+            if(game.getGhostEdibleTime(ghost) > 0){
+                if(game.getShortestPathDistance(msPLocation, game.getGhostCurrentNodeIndex(ghost)) < closestGhostDistance){
+                    closestEdibleGhost = ghost;
+                    closestGhostDistance = game.getShortestPathDistance(msPLocation, game.getGhostCurrentNodeIndex(ghost));
+                }
+            }
+        }
+        // If there is an edible ghost, snack.
+        if(closestEdibleGhost != null) {
+            return game.getNextMoveTowardsTarget(msPLocation,
+                    game.getGhostCurrentNodeIndex(closestEdibleGhost), DM.PATH);
+        }
+        //
+        EAT PILLS
+        //
+        // If the above two sections don't return anything, we want to return an action to go for pills.
+        return routeToPills(game, msPLocation);
+    }
+
+    private MOVE routeToPills(Game game, int msPLocation) {
+        ArrayList<Integer> nextPill = new ArrayList<>();
+        int[] pills = game.getActivePillsIndices();
+        int[] powerUps = game.getActivePowerPillsIndices();
+        for (int i = 0; i < pills.length; i++) {
+            if (game.isPillStillAvailable(i)) {
+                nextPill.add(pills[i]);
+            }
+        }
+        for (int i = 0; i < powerUps.length; i++) {
+            if (game.isPowerPillStillAvailable(i)) {
+                nextPill.add(powerUps[i]);
+            }
+        }
+        int[] targets = new int[nextPill.size()];
+        for (int i = 0; i < targets.length; i++) {
+            targets[i] = nextPill.get(i);
+        }
+        if (game.isJunction(msPLocation)) {
+            Game copy = game.copy();
+            Maze maze = copy.getCurrentMaze();
+            int cost = 0;
+            PriorityQueue<Integer> frontier = new PriorityQueue<>();
+            frontier.add(msPLocation);
+            while(!frontier.isEmpty()){
+                int[] neighbours = copy.getNeighbouringNodes(frontier.peek());
+                if(frontier.peek() == targets[0]) {
+                    break;
+                }
+                for(int entry : neighbours){
+                }
+            }
+            return game.getNextMoveTowardsTarget(msPLocation,
+                    game.getClosestNodeIndexFromNodeIndex(msPLocation, targets, DM.PATH),
+                    DM.PATH);
+        } else {
+            return game.getNextMoveTowardsTarget(msPLocation,
+                    game.getClosestNodeIndexFromNodeIndex(msPLocation, targets, DM.PATH),
+                    DM.PATH);
+        }
+    }
+
+    */
+
+
+
 //    private ArrayList<Integer> routeToPills(Game game, int msPLocation) {
 //        ArrayList<Integer> allPills = new ArrayList<>();
 //        int[] pills = game.getActivePillsIndices();
